ee2e92e934844314e0fef26c15a5f089ab02ec81
FarimaKafi <126360004+FarimaKafi@users.noreply.github.com>
2023-05-12 22:50:07 +0330

    do calculate method

--- src/main/java/sbu/cs/CalculatePi/PiCalculator.java
+++ src/main/java/sbu/cs/CalculatePi/PiCalculator.java
@@ -1,5 +1,11 @@
 package sbu.cs.CalculatePi;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class PiCalculator {
 
     /**
@@ -15,10 +21,43 @@ public class PiCalculator {
      * @return pi in string format (the string representation of the BigDecimal object)
      */
 
+    private static final int THREAD_COUNT = 10;
+
     public String calculate(int floatingPoint)
     {
-        // TODO
-        return null;
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
+        BigDecimal pi = BigDecimal.ZERO;
+
+        for (int k = 0; k < 100; k++) {
+            int finalK = k;
+            executorService.execute(() -> {
+                BigDecimal term = BigDecimal.valueOf(0);
+                int scale = floatingPoint + 10;
+                int iterationLimit = finalK == 0 ? 1 : (int) Math.pow(2, finalK + 1);
+
+                for (int i = 0; i < iterationLimit; i++) {
+                    BigDecimal numerator = BigDecimal.valueOf(4).pow(finalK).multiply(BigDecimal.valueOf(8 * i + 1))
+                            .subtract(BigDecimal.valueOf(2).multiply(BigDecimal.valueOf(4 * i + 2)).multiply(BigDecimal.valueOf(2).pow(finalK)))
+                            .subtract(BigDecimal.valueOf(1).multiply(BigDecimal.valueOf(8 * i + 4)).multiply(BigDecimal.valueOf(2).pow(2 * finalK)))
+                            .subtract(BigDecimal.valueOf(1).multiply(BigDecimal.valueOf(8 * i + 5)).multiply(BigDecimal.valueOf(2).pow(finalK)))
+                            .subtract(BigDecimal.valueOf(1).multiply(BigDecimal.valueOf(8 * i + 6)).multiply(BigDecimal.valueOf(2).pow(finalK)));
+                    BigDecimal denominator = BigDecimal.valueOf(16).pow(finalK).multiply(BigDecimal.valueOf(i).add(BigDecimal.valueOf(1)))
+                            .multiply(BigDecimal.valueOf(8).multiply(BigDecimal.valueOf(i)).add(BigDecimal.valueOf(7)));
+                    term = term.add(numerator.divide(denominator, scale, BigDecimal.ROUND_HALF_UP));
+                }
+
+                pi = pi.add(term);
+            });
+        }
+
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return pi.setScale(floatingPoint, BigDecimal.ROUND_HALF_UP).toString();
     }
 
     public static void main(String[] args) {
